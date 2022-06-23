ee0742d690bedbb99baaa84c9a9a63ed0e778a6b
João Oliveira <joaomarcosnet10@gmail.com>
2022-06-23 15:10:21 -0300

    fix dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int MAX = 1e5; 
 typedef pair<int,int> ii;
 
-vector<vector<pair<int, int>>> Adj(MAX); 
+vector<vector<ii>> Adj(MAX); 
 int D[MAX]; 
 priority_queue<ii, vector<ii>, greater<ii>> pq; 
 
@@ -14,14 +14,21 @@ int main() {
         D[i] = INT_MAX; 
     } 
 
+    int nodes, ed; cin >> nodes >> ed;
+
+    for(int i = 0; i < ed; i++){
+        int a,b,c; cin >> a >> b >> c;
+        Adj[a].push_back({c,b});
+        Adj[b].push_back({c,a});
+    }
+
     int begin_from = 1;
 
     pq.push({0, begin_from}); 
     while(!pq.empty()) {
-        pair<int, int> temp = pq.top(); 
+        int u = pq.top().second; 
+        int w = pq.top().first; 
         pq.pop(); 
-        int u = temp.second; 
-        int w = temp.first; 
         if (w < D[u]) {
             D[u] = w;
             for (int i = 0; i < Adj[u].size(); i++) {
@@ -29,4 +36,9 @@ int main() {
             } 
         } 
     } 
+
+    for(int i = 1; i < nodes; i++){
+        cout << D[i] << ' ';
+    }
+    cout << endl;
 } 
