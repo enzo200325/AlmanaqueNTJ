857c1adede550e5d6c82e7896cd15298f780555e
João Oliveira <joaomarcosnet10@gmail.com>
2023-03-08 10:05:47 -0300

    fenwick

--- codigos/Estruturas/fenwick.cpp
+++ codigos/Estruturas/fenwick.cpp
@@ -1,13 +1,23 @@
 // Fenwick Tree
 //
 // Processas queries de operacao com inverso
-// em O(log(n)) e faz updates em O(log(n))
+// Build: O(n)
+// Query: O(log(n))
+// Update: O(log(n))
 
 typedef long long ll;
 
 struct fenwick {
     vector<ll> bit;
-    fenwick (int n) { bit.assign(n+1, 0); }
+    fenwick(int n) { bit.assign(n+1, 0); }
+    fenwick(vector<ll>& v) {
+        int n = v.size();
+        bit.assign(n+1, 0);
+        for(int i = 1; i <= n; i++) bit[i] = v[i-1];
+        for(int i = 1, j = 2; i <= n; i++, j = i + (i & -i)) if(j <= n) {
+            bit[j] += bit[i];
+        }
+    }
     ll query(int i){
         ll res = 0;
         for(; i; i -= (i & -i))
