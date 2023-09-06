6857319a6a67746e4a9bc187783d141e90a93208
João Oliveira <joaomarcosnet10@gmail.com>
2023-09-06 14:43:31 -0300

    Melhor formatado

--- codigos/Geometria/convex_hull.cpp
+++ codigos/Geometria/convex_hull.cpp
@@ -3,42 +3,40 @@
 // Algoritmo Graham's Scan
 // Complexidade: O(n log(n))
 
-bool ccw(pt& p, pt& a, pt& b, bool collinear = 0) {
-	pt p1 = a - p;
-	pt p2 = b - p;
-	return collinear ? (p2 ^ p1) <= 0 : (p2 ^ p1) < 0;
+bool ccw(pt &p, pt &a, pt &b, bool collinear = 0) {
+    pt p1 = a - p;
+    pt p2 = b - p;
+    return collinear ? (p2 ^ p1) <= 0 : (p2 ^ p1) < 0;
 }
 
 vector<pt> convex_hull(vector<pt> v, bool collinear = 0) {
     int n = size(v);
     pt p0 = v[0];
-	for (int i = 1; i < n; i++) {
-		if (v[i] < p0) p0 = v[i];
-	}
-
-	sort(begin(v), end(v), [&] (pt &l, pt &r) { // sorta clockwise
-			pt p1 = l - p0;
-			pt p2 = r - p0;
-			ll c1 = p1 ^ p2;
-			return c1 < 0 || ((c1 == 0) && p0.dist2(l) < p0.dist2(r));
-	});
-
-    if (collinear) {
-        int IDX = n;
-        for (int i = n - 2; i >= 0; i--) { // reverte o ultimo lado do poligono
-            if (ccw(v[0], v[n - 1], v[i])) {
-                IDX = i + 1; break;
-            }
-        }
-        reverse(begin(v) + IDX, end(v));
+    for (int i = 1; i < n; i++) {
+        if (v[i] < p0) p0 = v[i];
     }
 
-	vector<pt> ch {p0, v[1]};
-	for (int i = 2; i < n; i++) {
-		while (ch.size() > 2 && (ccw(ch.end()[-2], ch.end()[-1], v[i], !collinear)))
-			ch.pop_back();
-		ch.emplace_back(v[i]);
-	}
+    sort(begin(v), end(v), [&](pt &l, pt &r) { // sorta clockwise
+        pt p1 = l - p0;
+        pt p2 = r - p0;
+        ll c1 = p1 ^ p2;
+        return c1 < 0 || ((c1 == 0) && p0.dist2(l) < p0.dist2(r));
+    });
+
+    if (collinear) {
+        for (int i = n - 2; i >= 0; i--) { // reverte o ultimo lado do poligono
+            if (ccw(v[0], v[n - 1], v[i])) {
+                reverse(begin(v) + i + 1, end(v));
+                break;
+            }
+        }
+    }
+
+    vector<pt> ch{p0, v[1]};
+    for (int i = 2; i < n; i++) {
+        while (ch.size() > 2 && (ccw(ch.end()[-2], ch.end()[-1], v[i], !collinear))) ch.pop_back();
+        ch.emplace_back(v[i]);
+    }
 
     return ch;
 }
--- codigos/Geometria/pt.cpp
+++ codigos/Geometria/pt.cpp
@@ -3,26 +3,28 @@
 // Ponto com coordenadas inteiras e alguns metodos
 
 struct pt {
-    ll x, y;
-    pt(): x(0),y(0) { }
-    pt(ll _x, ll _y): x(_x),y(_y) {}
+	ll x, y;
+	pt() : x(0), y(0) {}
+	pt(ll _x, ll _y) : x(_x), y(_y) {}
 
-    friend pt operator * (const pt& a, const ll& b) { return pt(b * a.x, b * a.y); }
-    friend pt operator - (const pt& a, const pt& b) { return pt(a.x - b.x, a.y - b.y); }
-    friend pt operator + (const pt& a, const pt& b) { return pt(a.x + b.x, a.y + b.y); }
-    friend ll operator * (const pt& a, const pt& b) { return a.x * b.x + a.y * b.y; }
-    friend ll operator ^ (const pt& a, const pt& b) { return a.x * b.y - a.y * b.x; }
+	friend pt operator*(const pt &a, const ll &b) { return pt(b * a.x, b * a.y); }
+	friend pt operator-(const pt &a, const pt &b) { return pt(a.x - b.x, a.y - b.y); }
+	friend pt operator+(const pt &a, const pt &b) { return pt(a.x + b.x, a.y + b.y); }
+	friend ll operator*(const pt &a, const pt &b) { return a.x * b.x + a.y * b.y; }
+	friend ll operator^(const pt &a, const pt &b) { return a.x * b.y - a.y * b.x; }
 
-    bool operator < (const pt& p) {
+	bool operator<(const pt &p) {
 		if (x == p.x) return y < p.y;
 		return x < p.x;
 	}
-	ll dist2(const pt& p) {
+	ll dist2(const pt &p) {
 		ll dx = x - p.x;
 		ll dy = y - p.y;
 		return dx * dx + dy * dy;
 	}
 
-    friend ostream& operator << (ostream& out, const pt& a) { return out << "(" << a.x << "," << a.y << ")"; }
-    friend istream& operator >> (istream& in, pt& a) { return in >> a.x >> a.y; }
+	friend ostream &operator<<(ostream &out, const pt &a) {
+		return out << "(" << a.x << "," << a.y << ")";
+	}
+	friend istream &operator>>(istream &in, pt &a) { return in >> a.x >> a.y; }
 };
