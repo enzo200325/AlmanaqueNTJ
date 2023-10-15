9a33f4f00cc5663e686abb54c25c32721f9f05c7
João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
2023-10-15 18:40:12 +0000

    Update stress.sh

--- codigos/Extra/Stress Test/stress.sh
+++ codigos/Extra/Stress Test/stress.sh
@@ -1,9 +1,9 @@
 // Script de stress test
 
 set -e
-g++-13 -O2 code.cpp -o code
-g++-13 -O2 brute.cpp -o brute
-g++-13 -O2 gen.cpp -o gen
+g++ -O2 code.cpp -o code
+g++ -O2 brute.cpp -o brute
+g++ -O2 gen.cpp -o gen
 
 for((i = 1; ; ++i)); do
     ./gen > in
