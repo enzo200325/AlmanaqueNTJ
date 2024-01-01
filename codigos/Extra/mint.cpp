840f889a2e73f38f9aebd050f1ef359c7cf1e12a
João Oliveira <joaomarcosnet10@gmail.com>
2023-12-31 21:20:55 -0300

    pdf e mint

--- codigos/Extra/mint.cpp
+++ codigos/Extra/mint.cpp
@@ -4,13 +4,20 @@
 
 template<int mod> struct Mint {
     int val;
-    Mint(ll v = 0) { val = v % mod; if (val < 0) val += mod; }
+    Mint() : val(0) { }
+    Mint(ll v) {
+        if (v < -mod || v >= 2 * mod) v %= mod;
+        if (v >= mod) v -= mod;
+        if (v < 0) v += mod;
+        val = v;
+    }
     Mint pwr(Mint b, ll e) {
         Mint res;
         for (res = 1; e; e >>= 1, b = b * b) if (e & 1) res = res * b;
         return res;
     }
     bool operator==(Mint o) const { return val == o.val; }
+    bool operator!=(Mint o) const { return val != o.val; }
     bool operator<(Mint o) const { return val < o.val; }
     friend Mint operator*(Mint a, Mint o) { return (ll)a.val * o.val; }
     friend Mint operator+(Mint a, Mint o) {
